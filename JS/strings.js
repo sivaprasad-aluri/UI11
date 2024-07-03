011e992db5c446d732d2d56a705f2c1435a98cf0
avistut <alurishivaprasad1@gmail.com>
2024-07-03 12:16:11 +0530

    strings and variables first topic

---
+++ JS/strings.js
@@ -0,0 +1,23 @@
+// comments
+// Strings - single line
+
+/* 
+--multi line
+    hello 
+    World
+    React
+*/
+
+"One";
+"Hello";
+" ";
+"   sdfsafd   sdfsdf        ";
+"124";
+"##!!@@@";
+"";
+
+"orange";
+
+"orange";
+
+"orange";
---
+++ JS/variables.js
@@ -0,0 +1,12 @@
+var pen = "reynolds";
+
+var penDescription =
+  "i am using a pen which is cost of 100/- and the name of the pen is " + pen;
+
+var penDescription1 =
+  "i am using a pen which is cost of 100/- and the name of the pen is " + pen;
+
+var penDescription2 =
+  "i am using a pen which is cost of 100/- and the name of the pen is " + pen;
+
+console.log("penDescription", penDescription1);
